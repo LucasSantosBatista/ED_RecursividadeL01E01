78a9119be5efd0f97aa5f4a237ed93aa2e02d119
LucasSantosBatista <142185716+LucasSantosBatista@users.noreply.github.com>
2024-02-25 14:36:43 -0300

    Commit

--- src/controller/NaturalController.java
+++ src/controller/NaturalController.java
@@ -7,9 +7,13 @@ public class NaturalController {
 	
 	public int somaNatural(int num) {
 		if (num < 0) {
+			//Condição de parada: contagem está abaixo de zero (não é mais natural).
 			return 0;
 		} else {
+			//Exibe teste
 			System.out.println(num);
+			
+			//Recursiva somando total + (total - 1)
 			return num + somaNatural(num - 1);
 		}
 	}
--- src/view/Main.java
+++ src/view/Main.java
@@ -4,9 +4,13 @@ import controller.NaturalController;
 
 public class Main {
 	public static void main(String[] args) {
+		//Quantidade de números naturais
 		int num = 10;
 		
+		//Definindo a classe de metodos
 		NaturalController nCont = new NaturalController();
+		
+		//Leva variável pela função, retorna e exibe 
 		int soma = nCont.somaNatural(num);
 		System.out.println("Quantidade: " + num + " números naturais | Somatória: " + soma);
 	}
